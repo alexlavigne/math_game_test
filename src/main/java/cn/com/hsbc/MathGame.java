357cdb4cff38a96e607a26d063e13fc7f782daa8
AlexLavinge <alexlavigne@163.com>
2018-06-22 15:42:24 +0800

    to  fix new requirement of stage2

--- src/main/java/cn/com/hsbc/MathGame.java
+++ src/main/java/cn/com/hsbc/MathGame.java
@@ -10,21 +10,17 @@ public class MathGame {
 
 
     private StringBuffer sb;
-    private int remainder_3;
-    private int remainder_5;
 
     public MathGame() {
         sb = new StringBuffer();
     }
 
-    public String getResultByNumber(int i) {
+    public String getResultByNumberStage1(int i) {
         sb.setLength(0);
-        remainder_3 = i % 3;
-        remainder_5 = i % 5;
-        if (remainder_3 == 0) {
+        if (isDivisedBy3(i)) {
             sb.append("Fizz");
         }
-        if (remainder_5 == 0) {
+        if (isDivisedBy5(i)) {
             sb.append("Buzz");
         }
         if (sb.length()==0){
@@ -33,12 +29,65 @@ public class MathGame {
         return sb.toString();
     }
 
+    public boolean isDivisedBy3(int i) {
+        return i % 3 == 0;
+    }
 
-    public List<String> getHundredResults() {
+
+    public boolean isDivisedBy5(int i) {
+        return i % 5 == 0;
+    }
+
+
+    public String getResultByNumberStage2(int i) {
+        sb.setLength(0);
+        if (isDivisedBy3(i)||isContains3(i)) {
+            sb.append("Fizz");
+        }
+        if (isDivisedBy5(i)||isContains5(i)) {
+            sb.append("Buzz");
+        }
+        if (sb.length()==0){
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+
+    public boolean isContains3(int i) {
+        return (i+"").contains("3");
+    }
+
+
+    public boolean isContains5(int i) {
+        return (i+"").contains("5");
+    }
+
+
+
+    public List<String> getHundredResultsStage1() {
+        System.out.println();
+        System.out.println("******* start print result of stage1 *********");
+        System.out.println();
+        System.out.println();
         List<String> tmpList = new ArrayList<String>();
         String tmpNumber;
         for (int i = 1; i < 101; i++) {
-            tmpNumber = getResultByNumber(i);
+            tmpNumber = getResultByNumberStage1(i);
+            System.out.println(tmpNumber);
+            tmpList.add(tmpNumber);
+        }
+        return tmpList;
+    }
+
+    public List<String> getHundredResultsStage2() {
+        System.out.println();
+        System.out.println("******* start print result of stage2 *********");
+        System.out.println();
+        System.out.println();
+        List<String> tmpList = new ArrayList<String>();
+        String tmpNumber;
+        for (int i = 1; i < 101; i++) {
+            tmpNumber = getResultByNumberStage2(i);
             System.out.println(tmpNumber);
             tmpList.add(tmpNumber);
         }
--- src/test/java/cn/com/hsbc/MathGameTest.java
+++ src/test/java/cn/com/hsbc/MathGameTest.java
@@ -1,9 +1,9 @@
 package cn.com.hsbc;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,20 +11,74 @@ import java.util.List;
  */
 public class MathGameTest {
 
+    private MathGame mathGame;
+    
+    @Before
+    public void setUp() throws Exception {
+        mathGame = new MathGame(); 
+    }
     @Test
-    public void testGetResult() {
-        MathGame mathGame = new MathGame();
-        Assert.assertEquals("1",mathGame.getResultByNumber(1));
-        Assert.assertEquals("Fizz",mathGame.getResultByNumber(3));
-        Assert.assertEquals("Buzz",mathGame.getResultByNumber(5));
-        Assert.assertEquals("Fizz",mathGame.getResultByNumber(6));
-        Assert.assertEquals("FizzBuzz",mathGame.getResultByNumber(15));
+    public void isDivisedBy3() throws Exception {
+        Assert.assertEquals(false,mathGame.isDivisedBy3(1));
+        Assert.assertEquals(true,mathGame.isDivisedBy3(3));
+        Assert.assertEquals(true,mathGame.isDivisedBy3(24));
     }
 
     @Test
-    public void testGetHundredResult() {
+    public void isDivisedBy5() throws Exception {
+        Assert.assertEquals(false,mathGame.isDivisedBy5(1));
+        Assert.assertEquals(true,mathGame.isDivisedBy5(5));
+        Assert.assertEquals(true,mathGame.isDivisedBy5(25));
+    }
+
+    @Test
+    public void getResultByNumberStage1() throws Exception {
+        Assert.assertEquals("1",mathGame.getResultByNumberStage1(1));
+        Assert.assertEquals("Fizz",mathGame.getResultByNumberStage1(3));
+        Assert.assertEquals("Buzz",mathGame.getResultByNumberStage1(5));
+        Assert.assertEquals("Fizz",mathGame.getResultByNumberStage1(6));
+        Assert.assertEquals("FizzBuzz",mathGame.getResultByNumberStage1(15));
+    }
+
+
+    @Test
+    public void getHundredResultsStage1() throws Exception {
         MathGame mathGame = new MathGame();
-        List<String> tmpList = mathGame.getHundredResults();
+        List<String> tmpList = mathGame.getHundredResultsStage1();
+        Assert.assertEquals(100, tmpList.size());
+    }
+
+    @Test
+    public void isContains3() throws Exception {
+        Assert.assertEquals(false,mathGame.isContains3(1));
+        Assert.assertEquals(true,mathGame.isContains3(3));
+        Assert.assertEquals(true,mathGame.isContains3(23));
+    }
+
+    @Test
+    public void isContains5() throws Exception {
+        Assert.assertEquals(false,mathGame.isContains5(1));
+        Assert.assertEquals(true,mathGame.isContains5(5));
+        Assert.assertEquals(true,mathGame.isContains5(51));
+    }
+
+    @Test
+    public void getResultByNumberStage2() throws Exception {
+        Assert.assertEquals("1",mathGame.getResultByNumberStage2(1));
+        Assert.assertEquals("Fizz",mathGame.getResultByNumberStage2(3));
+        Assert.assertEquals("Buzz",mathGame.getResultByNumberStage2(5));
+        Assert.assertEquals("Fizz",mathGame.getResultByNumberStage2(6));
+        Assert.assertEquals("FizzBuzz",mathGame.getResultByNumberStage2(15));
+        Assert.assertEquals("FizzBuzz",mathGame.getResultByNumberStage2(53));
+        Assert.assertEquals("FizzBuzz",mathGame.getResultByNumberStage2(57));
+        Assert.assertEquals("FizzBuzz",mathGame.getResultByNumberStage2(35));
+    }
+
+
+    @Test
+    public void getHundredResultsStage2() throws Exception {
+        MathGame mathGame = new MathGame();
+        List<String> tmpList = mathGame.getHundredResultsStage2();
         Assert.assertEquals(100, tmpList.size());
     }
 
