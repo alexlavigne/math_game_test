c59c385beb7d7c06b1a0e11ed7d2556569a321f5
AlexLavinge <alexlavigne@163.com>
2018-06-22 15:57:32 +0800

    to  fix new requirement of stage2

--- src/main/java/cn/com/hsbc/MathGame.java
+++ src/main/java/cn/com/hsbc/MathGame.java
@@ -8,6 +8,47 @@ import java.util.List;
  */
 public class MathGame {
 
+    /**
+     *Prints the numbers from 1 to 100 .
+     * But for multiples of the three print "Fizz" instead of the number and for the
+     * multiples of five print "Buzz".For numbers which multiples of both three and five
+     * print "FizzBuzz"
+     * */
+    public List<String> getHundredResultsStage1() {
+        System.out.println();
+        System.out.println("******* start print result of stage1 *********");
+        System.out.println();
+        System.out.println();
+        List<String> tmpList = new ArrayList<String>();
+        String tmpNumber;
+        for (int i = 1; i < 101; i++) {
+            tmpNumber = getResultByNumberStage1(i);
+            System.out.println(tmpNumber);
+            tmpList.add(tmpNumber);
+        }
+        return tmpList;
+    }
+
+    /**
+     *Prints the numbers from 1 to 100 .
+     * But if a Number divisible by 3 or if it has a 3 in it then use "Fizz" to replace it .
+     *  If a Number divisible by 5 or if it has a 5 in it then use "Buzz" to replace it .
+     *  If a Number matches both then use "FizzBuzz" to replace it .
+     * */
+    public List<String> getHundredResultsStage2() {
+        System.out.println();
+        System.out.println("******* start print result of stage2 *********");
+        System.out.println();
+        System.out.println();
+        List<String> tmpList = new ArrayList<String>();
+        String tmpNumber;
+        for (int i = 1; i < 101; i++) {
+            tmpNumber = getResultByNumberStage2(i);
+            System.out.println(tmpNumber);
+            tmpList.add(tmpNumber);
+        }
+        return tmpList;
+    }
 
     private StringBuffer sb;
 
@@ -63,34 +104,4 @@ public class MathGame {
     }
 
 
-
-    public List<String> getHundredResultsStage1() {
-        System.out.println();
-        System.out.println("******* start print result of stage1 *********");
-        System.out.println();
-        System.out.println();
-        List<String> tmpList = new ArrayList<String>();
-        String tmpNumber;
-        for (int i = 1; i < 101; i++) {
-            tmpNumber = getResultByNumberStage1(i);
-            System.out.println(tmpNumber);
-            tmpList.add(tmpNumber);
-        }
-        return tmpList;
-    }
-
-    public List<String> getHundredResultsStage2() {
-        System.out.println();
-        System.out.println("******* start print result of stage2 *********");
-        System.out.println();
-        System.out.println();
-        List<String> tmpList = new ArrayList<String>();
-        String tmpNumber;
-        for (int i = 1; i < 101; i++) {
-            tmpNumber = getResultByNumberStage2(i);
-            System.out.println(tmpNumber);
-            tmpList.add(tmpNumber);
-        }
-        return tmpList;
-    }
 }
